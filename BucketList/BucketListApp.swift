214489af5657b11a27cae9883b2d15e357092cc0
Brandon Glenn <Brandon.Glenn@icloud.com>
2022-01-11 09:25:15 -0700

    Initial Commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Brandon Glenn on 1/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
